2ce6f4fe2e529e10c8c34cbc1839f11533568c31
Leyla Vasquez <Leylagisela@gmail.com>
2023-09-19 18:32:13 -0600

    Se agregaron varios usuarios

--- data2.js
+++ data2.js
@@ -1,15 +1,34 @@
 const users = [
-    {
-      email: "leylagisela@gmail.com",
-      user: "legive",
-      password: "1234",
-      rol: "admin",
-    },
-    {
-      email: "interra_2012@yahoo.com",
-      user: "interra",
-      password: "12345",
-      rol: "user",
-    },
-  ];
+  {
+    email: "leylagisela@gmail.com",
+    user: "legive",
+    password: "1234",
+    rol: "admin"
+  },
+  {
+    email: "usuario1@example.com",
+    user: "usuario1",
+    password: "password1",
+    rol: "user"
+  },
+  {
+    email: "usuario2@example.com",
+    user: "usuario2",
+    password: "password2",
+    rol: "user"
+  },
+  {
+    email: "admin2@example.com",
+    user: "admin2",
+    password: "adminpass2",
+    rol: "admin"
+  },
+  {
+    email: "usuario3@example.com",
+    user: "usuario3",
+    password: "password3",
+    rol: "user"
+  }
+]
+ 
   module.exports=users
